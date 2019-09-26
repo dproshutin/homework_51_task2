7ee8b47d5382c8fc371b7868663089170abe9ae4
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2019-09-26 18:37:10 +0600

    The block about us is corrected.

--- src/App.js
+++ src/App.js
@@ -10,9 +10,14 @@ function App() {
     <div className="App">
         <Header/>
         <Promo image="uploads/img/slider_pic.jpg"/>
-        <Team image="uploads/img/employee1_pic.jpg" name="Craig Garner" position="Ceo" />
-        <Team image="uploads/img/employee2_pic.jpg" name="Bridge Roberts" position="Creative Genius" />
-        <Team image="uploads/img/employee3_pic.jpg" name="Malade Garner" position="Chef de projet" />
+        <div className="about_us">
+            <h3>Our Team</h3>
+            <div className="team container">
+                <Team image="uploads/img/employee1_pic.jpg" name="Craig Garner" position="Ceo" />
+                <Team image="uploads/img/employee2_pic.jpg" name="Bridge Roberts" position="Creative Genius" />
+                <Team image="uploads/img/employee3_pic.jpg" name="Malade Garner" position="Chef de projet" />
+            </div>
+        </div>
         <Footer/>
     </div>
   );
--- src/Team.js
+++ src/Team.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 const Team = (props) => {
     return (
-        <div className="team container">
         <div className="team_member">
             <img src={props.image} width="258" height="271"/>
             <h2>{props.name}</h2>
             <p>{props.position}</p>
-        </div>
         </div>
     );
 };
